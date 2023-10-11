a70d1c665007105523cbd5f7c1e7f5859836e590
Tamas Szabo <szabototo89@gmail.com>
2023-10-11 23:19:51 +0100

    Create message board feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
 import { MessageBoardCard } from "@/components/MessageBoardCard";
-import { db } from "@/db/schema";
+import { db, MessageBoardSchema } from "@/db/schema";
 import { MessageBoards } from "@/db/schemas/messageBoards.schema";
+import { Button, Textarea, TextInput } from "@mantine/core";
+import { zfd } from "zod-form-data";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
 
 export default async function Home() {
-  const messageBoards = await db.select().from(MessageBoards);
+  const messageBoards = z
+    .array(MessageBoardSchema)
+    .parse(await db.select().from(MessageBoards));
+
+  const createMessageBoard = async (formData: FormData) => {
+    "use server";
+
+    const CreateMessageBoardFormDataSchema = zfd.formData({
+      title: zfd.text(),
+      description: zfd.text(),
+    });
+    const input = CreateMessageBoardFormDataSchema.parse(formData);
+
+    await db.insert(MessageBoards).values({
+      status: "draft",
+      ...input,
+    });
+
+    revalidatePath("/");
+  };
 
   return (
     <>
+      <form action={createMessageBoard}>
+        <TextInput name="title" label="Title" />
+        <Textarea name="description" label="Description" />
+        <Button type="submit">Submit</Button>
+      </form>
+
       {messageBoards.map((messageBoard) => (
         <MessageBoardCard key={messageBoard.id} messageBoard={messageBoard} />
       ))}
