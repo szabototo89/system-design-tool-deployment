b1007fb65e6f7b51bf60c2d253e997bb3969e13c
Tamas Szabo <szabototo89@gmail.com>
2023-10-18 20:48:38 +0100

    Add devtools page to the project

---
+++ src/app/devtools/page.tsx
@@ -0,0 +1,61 @@
+import { readSeedFile, seedDatabase } from "@/db/seed-script-utils";
+import { ActionButton } from "@/components/ActionButton";
+import {
+  Button,
+  Card,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+
+export default function DevToolsPage() {
+  const resetDatabase = async () => {
+    "use server";
+    const seedFile = readSeedFile("src/db/seeds/real-life-seed.json");
+    await seedDatabase(seedFile);
+  };
+
+  return (
+    <Stack>
+      <Title>Development tools</Title>
+
+      <SimpleGrid cols={2}>
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+          <Text fw={500}>Database tools</Text>
+
+          <Text size="sm" c="dimmed">
+            Provides two essential functions: Drizzle Studio for easy database
+            management, and a quick reset option with test data for streamlined
+            testing and development. Manage your database effortlessly in one
+            place.
+          </Text>
+
+          <Group>
+            <Button
+              component="a"
+              target="_blank"
+              href="http://localhost:4983/"
+              variant="light"
+              color="blue"
+              mt="md"
+              radius="md"
+            >
+              Open Drizzle Studio
+            </Button>
+            <ActionButton
+              variant="outline"
+              color="red"
+              mt="md"
+              radius="md"
+              onClick={resetDatabase}
+            >
+              Reset database
+            </ActionButton>
+          </Group>
+        </Card>
+      </SimpleGrid>
+    </Stack>
+  );
+}
---
+++ src/db/seed-script-utils.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import {
+  MessageBoards,
+  MessageBoardSchema,
+} from "./schemas/messageBoards.schema";
+import { MessageInsertSchema, Messages } from "./schemas/messages.schema";
+import fs, { PathOrFileDescriptor } from "fs";
+import { db } from "./schema";
+
+const SeedFileSchema = z.object({
+  messageBoards: z.array(MessageBoardSchema),
+  messages: z.array(MessageInsertSchema),
+});
+type SeedFile = z.infer<typeof SeedFileSchema>;
+
+export function readSeedFile(seedFilePath: PathOrFileDescriptor): SeedFile {
+  const fileContent = fs.readFileSync(seedFilePath, "utf-8");
+  const rawFileContentAsJson = JSON.parse(fileContent);
+
+  return SeedFileSchema.parse(rawFileContentAsJson);
+}
+
+export async function seedDatabase(seedFile: SeedFile) {
+  await db.delete(Messages);
+  await db.delete(MessageBoards);
+  await db.insert(MessageBoards).values(seedFile.messageBoards);
+  await db.insert(Messages).values(seedFile.messages);
+}
